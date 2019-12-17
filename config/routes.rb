b60f0010bf2a70942b82131ae912194e7d1f7123
KleberDevMen <klebersubcontas@gmail.com>
2019-12-17 10:21:09 -0300

    exibir_chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contacts, only: %i(index new create destroy)
+  resources :chats, only: %i(index show)
 
   root to: "home#index"
 end
